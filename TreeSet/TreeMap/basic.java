0ca7ed131ca3c8468c3d8cd6549f0e8c29abc628
priyanshu639 <priyanshu63930@gmail.com>
2025-06-11 11:33:15 +0530

    treemap

---
+++ TreeSet/TreeMap/basic.java
@@ -0,0 +1,17 @@
+// package TreeSet.TreeMap;
+import java.util.*;
+
+
+public class basic {
+
+
+    public static void main(String[] args) {
+        TreeMap<String ,Integer> map=new TreeMap();
+        map.put("ram", 2);
+        map.put("sam1", 21);
+        map.put("kam2", 23);
+        map.put("zam3", 24);
+        map.put("aam4", 25);
+        System.out.println(map);
+    }
+}
---
+++ TreeSet/TreeMap/basic2.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class basic2 {
+
+
+    public static void main(String[] args) {
+        TreeSet<Integer> map=new TreeSet();
+        map.add( 42);
+        map.add( 21);
+        map.add( 20);
+        map.add( 24);
+        map.add( 25);
+        System.out.println(map);
+    }
+}
