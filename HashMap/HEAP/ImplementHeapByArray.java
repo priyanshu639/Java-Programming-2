b5f9ad816bb40bd72bb3377c0804ea1e315264fc
priyanshu639 <priyanshu63930@gmail.com>
2025-09-06 00:52:31 +0530

    implementation of MinHeap by array

---
+++ HashMap/HEAP/ImplementHeapByArray.java
@@ -0,0 +1,62 @@
+class MinHeap{
+    int arr[];
+    int size;
+
+    public MinHeap(int capacity) {
+        arr=new int[capacity];
+        size=0;
+    }
+    public void add(int num){
+        if(size==arr.length) return ;
+        arr[size]=num;
+        size++;
+        upheapify(size-1);
+
+    }
+    public void swap(int indx,int parent){
+        int temp=arr[indx];
+        arr[indx]=arr[parent];
+        arr[parent]=temp;
+
+    }
+    public void upheapify(int indx){
+        if(indx==0) return;
+        int parent=(indx-1)/2;
+        if(arr[indx]< arr[parent]){
+            swap(indx,parent);
+            upheapify(parent);
+        }
+        
+
+    }
+    public int peek(){
+        // if(size==0){
+        //     System.out.println("no element");
+        // }
+        
+    
+        return arr[0];
+        
+    }
+    public int size(){
+        return size;
+    }
+
+}
+public class ImplementHeapByArray {
+    public static void main(String[] args) {
+        MinHeap pq=new MinHeap(10);
+        System.out.println(pq.size());
+
+        pq.add(2);
+        pq.add(12);
+        pq.add(22);
+        pq.add(5);
+       System.out.println(( pq.peek()));
+       pq.add(3);
+       pq.add(1);
+       System.out.println(( pq.peek()));
+        
+    }
+    
+}
