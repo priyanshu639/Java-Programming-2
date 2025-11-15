b77ae9c2fe3411a902c05162d55a20631208b8fc
priyanshu639 <priyanshu63930@gmail.com>
2025-11-16 02:10:53 +0530

    reversing string without builtin function

---
+++ Hashset/STRING/reverseStringBuilder.java
@@ -0,0 +1,20 @@
+class reverseStringBuilder{
+    public static void main(String[] args) {
+        StringBuilder sb=new StringBuilder("abcde");
+        int high=sb.length()-1;
+        int st=0;
+        sb.reverse();
+        System.out.println(sb);
+        while(st<high){
+            char temp=sb.charAt(st);
+            sb.setCharAt(st,sb.charAt(high));
+            sb.setCharAt(high,temp);
+            st++;
+            high--;
+
+
+
+        }
+        System.out.println(sb);
+    }
+}
