9e249ef9c83147cc4d4ad5db6da8ab5ee3e8e302
priyanshu639 <priyanshu63930@gmail.com>
2025-11-15 12:02:15 +0530

    charAt in string

---
+++ Hashset/STRING/chartAt.java
@@ -0,0 +1,12 @@
+
+
+public class chartAt {
+    public static void main(String[] args) {
+        String s="cdj ndkvmm";
+        System.out.println(s.charAt(6));
+        int n=s.length();
+        System.out.println(n);
+    }
+
+    
+}
