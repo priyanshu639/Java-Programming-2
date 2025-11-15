ac7c3c814168aa222e06e9e9641d84f69116773e
priyanshu639 <priyanshu63930@gmail.com>
2025-11-15 23:41:27 +0530

    toggle string

---
+++ Hashset/STRING/StringBuilder12.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class StringBuilder12 {
+    public static void main(String[] args) {
+        // StringBuilder sb =new StringBuilder(10);
+        // System.out.println(sb.length());
+        // System.out.println(sb.capacity());
+        Scanner sc=new Scanner(System.in);
+
+        StringBuilder sb =new StringBuilder(sc.nextLine());
+        for(int i=0;i<sb.length();i++){
+            char ch =sb.charAt(i);
+            int asci =(int)ch;
+            if(asci >= 97){
+                asci-=32;
+                char w=(char) asci;
+
+                sb.setCharAt(i, w);
+
+            }
+            else{
+                asci+=32;
+                char w=(char) asci;
+                
+                sb.setCharAt(i,w );
+            }
+        }
+        System.out.println(sb);
+
+       
+
+
+
+    }
+    
+}
