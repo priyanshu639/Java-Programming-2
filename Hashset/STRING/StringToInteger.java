9f4113df6d20560a9095b302097026ac5b312d8a
priyanshu639 <priyanshu63930@gmail.com>
2025-11-17 22:09:43 +0530

    string array me se max find karna

---
+++ Hashset/STRING/StringToInteger.java
@@ -0,0 +1,14 @@
+public class StringToInteger {
+    public static void main(String[] args) {
+        // String s[]={"asxds","dsffvec","dvfcc"};
+        String s[]={"1233","232435","33875924"};
+        int maxvalue=Integer.parseInt(s[0]);
+        for(int i=0;i<s.length;i++){
+            int m=Integer.parseInt(s[i]);
+            maxvalue=Math.max(maxvalue,m);
+            
+        }
+        System.out.println(maxvalue);
+    }
+    
+}
