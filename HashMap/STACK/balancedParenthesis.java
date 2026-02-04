54cfce6dd0b5d3669c97991146855de00c385fdb
priyanshu639 <priyanshu63930@gmail.com>
2026-02-04 23:03:19 +0530

    balenced parenthesis using stck

---
+++ HashMap/STACK/balancedParenthesis.java
@@ -0,0 +1,33 @@
+import java.util.*;
+public class balancedParenthesis {
+
+    public static boolean balenced(String str){
+        Stack<Character>st=new Stack<>();
+        int n=str.length();
+        for(int i=0;i<n;i++){
+            char ch=str.charAt(i);
+            if(ch=='('){
+                st.push(ch);
+            }
+            else{//
+                if(st.size()==0){
+                    return false;
+
+                } 
+                if(st.peek()=='(') st.pop();
+                else{
+                    return true;
+                }
+
+            }
+        }
+        if(st.size()>0) return  false;
+        else return true;
+
+    }
+    public static void main(String[] args) {
+        String str=")()((())";
+        System.out.println(balenced(str));
+    }
+    
+}
